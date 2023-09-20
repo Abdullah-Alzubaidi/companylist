8757e6eaec50b5ef519c992ec1604f1ce3c4c146
Abdullah-Alzubaidi <137665920+Abdullah-Alzubaidi@users.noreply.github.com>
2023-09-20 12:29:23 +0800

    Update map_area.py

--- map_area.py
+++ map_area.py
@@ -5,6 +5,7 @@ import warnings
 import pandas as pd
 import geopandas as gpd
 import folium
+import plotly.express as px
 from branca.element import Figure
 from shapely.geometry import Point
 
@@ -104,6 +105,52 @@ if __name__ == '__main__':
     #         # Add a click event to the marker
     #         marker.add_to(map_my)
     #         marker.add_child(folium.ClickForMarker(popup=update_sidebar))
+
+
+st.title("Company Per District")
+
+# Specify the file name
+file_name = "MMU ITP List 13_9_9_11.xlsx"
+
+try:
+    df = pd.read_excel(file_name, engine='openpyxl')
+except FileNotFoundError:
+    st.error(f"File '{file_name}' not found.")
+
+# Sidebar for state selection
+if df is not None:
+    selected_state = st.sidebar.selectbox("Select a State", df['STATE'].unique())
+else:
+    selected_state = None
+
+# Data processing
+if df is not None:
+    grouped_data = df.groupby(['STATE', 'CITY']).size().reset_index(name='CompanyCount')
+    grouped_data = grouped_data.sort_values(by=['STATE', 'CompanyCount'], ascending=[True, False])
+
+    # Create bar chart
+    if selected_state:
+        filtered_data = grouped_data[grouped_data['STATE'] == selected_state]
+        fig = px.bar(
+            filtered_data,
+            x='CompanyCount',
+            y='CITY',
+            orientation='h',
+            labels={'CITY': 'City', 'CompanyCount': 'Number of Companies'},
+            title=f'Company Distribution per City in {selected_state}'
+        )
+    else:
+        fig = px.bar(
+            grouped_data,
+            x='CompanyCount',
+            y='CITY',
+            orientation='h',
+            labels={'CITY': 'City', 'CompanyCount': 'Number of Companies'},
+            title='Company Distribution per City in Malaysia (Sorted by State and City)'
+        )
+
+    # Display the plotly figure
+    st.plotly_chart(fig)
     
     text_load_state.text('Plotting ... Done!')
 
