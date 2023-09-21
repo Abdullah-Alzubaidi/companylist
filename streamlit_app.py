1bdffe789e5bd7dda54c791c8ddce5a0f9ee12d4
zizo1964 <93069541+zizo1964@users.noreply.github.com>
2023-09-22 00:07:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ from streamlit_folium import st_folium
 from sidebar import sidebar
 import plotly.express as px
 
+st.image('logo-mmu.png', use_column_width=False, caption='', width=100)
 
 # 3.16000, 101.71000 : Kuala Lumpur
 
